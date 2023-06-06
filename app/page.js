081000e4006c71d3a75ed575ed7eaa945269643a
Utkarsh Bairagi <utkarshui1212@gmail.com>
2023-06-06 10:24:21 +0530

    "Home and footer naming adn style"

--- app/page.js
+++ app/page.js
@@ -1,25 +1,33 @@
 "use client";
+import Map from "@/components/Map";
 import SearchBar from "@/components/SearchBar";
 import TruckCards from "@/components/TruckCards";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+ 
+
+
   return (
     <section className="w-full h-full flex-col flex-center mt-4 gap-6">
       <div>
         <h1 className="head_text text-center">
           Experience Flavors
           <br />
-          <span className="orange_gradient text-center">with FoodWheels!!!</span>
+          <span className="orange_gradient text-center">
+            with FoodOnWheels!!!
+          </span>
         </h1>
         <p className="desc text-center">
           Truck Tracker: Hunt Down the Best Bites! Discover, Devour, and Delight
           in Food Truck Finds!
         </p>
       </div>
-      <div>
+      <div className="flex">
         <SearchBar />
-      </div>
+        </div>
+      {/* <Map /> */}
       <div>
         <h2 className="text-4xl font-bold text-center my-4">Our Centers</h2>
         <TruckCards />
